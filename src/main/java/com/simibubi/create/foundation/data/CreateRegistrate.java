eb488897ec532ef26074d6b551effca10d78724f
bladenowain-arch <bladenowain@gmail.com>
2026-09-01 21:08:43 +0100

    Port CreateRegistrate fluid identifiers to 26.1 API

--- src/main/java/com/simibubi/create/foundation/data/CreateRegistrate.java
+++ src/main/java/com/simibubi/create/foundation/data/CreateRegistrate.java
@@ -40,8 +40,8 @@ import net.createmod.catnip.registry.RegisteredObjectsHelper;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -73,9 +73,6 @@ public class CreateRegistrate extends AbstractRegistrate<CreateRegistrate> {
 
 	public static CreateRegistrate create(String modid) {
 		CreateRegistrate registrate = new CreateRegistrate(modid);
-		// The registrate is registered here instead of in the constructor so that if a subclass
-		// overrides the addRegisterCallback to be dependent on some sort of state initialized in the constructor,
-		// it won't explode. The consequence is that subclasses must manually provide their registrate to the callback API
 		CreateRegistrateRegistrationCallback.provideRegistrate(registrate);
 		return registrate;
 	}
@@ -113,7 +110,6 @@ public class CreateRegistrate extends AbstractRegistrate<CreateRegistrate> {
 	protected <R, T extends R> RegistryEntry<R, T> accept(String name, ResourceKey<? extends Registry<R>> type, Builder<R, T, ?, ?> builder, NonNullSupplier<? extends T> creator, NonNullFunction<DeferredHolder<R, T>, ? extends RegistryEntry<R, T>> entryFactory) {
 		RegistryEntry<R, T> entry = super.accept(name, type, builder, creator, entryFactory);
 		if (type.equals(Registries.ITEM) && currentTooltipModifierFactory != null) {
-			// grab the factory here for the lambda, it can change between now and registration
 			Function<Item, TooltipModifier> factory = currentTooltipModifierFactory;
 			this.addRegisterCallback(name, Registries.ITEM, item -> {
 				TooltipModifier modifier = factory.apply(item);
@@ -127,139 +123,109 @@ public class CreateRegistrate extends AbstractRegistrate<CreateRegistrate> {
 	}
 
 	@Override
-	public <T extends BlockEntity> CreateBlockEntityBuilder<T, CreateRegistrate> blockEntity(String name,
-																							 BlockEntityFactory<T> factory) {
+	public <T extends BlockEntity> CreateBlockEntityBuilder<T, CreateRegistrate> blockEntity(String name, BlockEntityFactory<T> factory) {
 		return blockEntity(self(), name, factory);
 	}
 
 	@Override
-	public <T extends BlockEntity, P> CreateBlockEntityBuilder<T, P> blockEntity(P parent, String name,
-																				 BlockEntityFactory<T> factory) {
+	public <T extends BlockEntity, P> CreateBlockEntityBuilder<T, P> blockEntity(P parent, String name, BlockEntityFactory<T> factory) {
 		return (CreateBlockEntityBuilder<T, P>) entry(name,
 			(callback) -> CreateBlockEntityBuilder.create(this, parent, name, callback, factory));
 	}
 
 	@Override
-	public <T extends Entity> CreateEntityBuilder<T, CreateRegistrate> entity(String name,
-																			  EntityType.EntityFactory<T> factory, MobCategory classification) {
+	public <T extends Entity> CreateEntityBuilder<T, CreateRegistrate> entity(String name, EntityType.EntityFactory<T> factory, MobCategory classification) {
 		return this.entity(self(), name, factory, classification);
 	}
 
 	@Override
-	public <T extends Entity, P> CreateEntityBuilder<T, P> entity(P parent, String name,
-																  EntityType.EntityFactory<T> factory, MobCategory classification) {
+	public <T extends Entity, P> CreateEntityBuilder<T, P> entity(P parent, String name, EntityType.EntityFactory<T> factory, MobCategory classification) {
 		return (CreateEntityBuilder<T, P>) this.entry(name, (callback) -> {
 			return CreateEntityBuilder.create(this, parent, name, callback, factory, classification);
 		});
 	}
 
-	// custom types
-
 	public <T extends MountedItemStorageType<?>> SimpleBuilder<MountedItemStorageType<?>, T, CreateRegistrate> mountedItemStorage(String name, Supplier<T> supplier) {
-		return this.entry(name, callback -> new SimpleBuilder<>(
-			this, this, name, callback, CreateRegistries.MOUNTED_ITEM_STORAGE_TYPE, supplier
-		).byBlock(MountedItemStorageType.REGISTRY));
+		return this.entry(name, callback -> new SimpleBuilder<>(this, this, name, callback, CreateRegistries.MOUNTED_ITEM_STORAGE_TYPE, supplier).byBlock(MountedItemStorageType.REGISTRY));
 	}
 
 	public <T extends MountedFluidStorageType<?>> SimpleBuilder<MountedFluidStorageType<?>, T, CreateRegistrate> mountedFluidStorage(String name, Supplier<T> supplier) {
-		return this.entry(name, callback -> new SimpleBuilder<>(
-			this, this, name, callback, CreateRegistries.MOUNTED_FLUID_STORAGE_TYPE, supplier
-		).byBlock(MountedFluidStorageType.REGISTRY));
+		return this.entry(name, callback -> new SimpleBuilder<>(this, this, name, callback, CreateRegistries.MOUNTED_FLUID_STORAGE_TYPE, supplier).byBlock(MountedFluidStorageType.REGISTRY));
 	}
 
 	public <T extends DisplaySource> SimpleBuilder<DisplaySource, T, CreateRegistrate> displaySource(String name, Supplier<T> supplier) {
-		return this.entry(name, callback -> new SimpleBuilder<>(
-			this, this, name, callback, CreateRegistries.DISPLAY_SOURCE, supplier
-		).byBlock(DisplaySource.BY_BLOCK).byBlockEntity(DisplaySource.BY_BLOCK_ENTITY));
+		return this.entry(name, callback -> new SimpleBuilder<>(this, this, name, callback, CreateRegistries.DISPLAY_SOURCE, supplier).byBlock(DisplaySource.BY_BLOCK).byBlockEntity(DisplaySource.BY_BLOCK_ENTITY));
 	}
 
 	public <T extends DisplayTarget> SimpleBuilder<DisplayTarget, T, CreateRegistrate> displayTarget(String name, Supplier<T> supplier) {
-		return this.entry(name, callback -> new SimpleBuilder<>(
-			this, this, name, callback, CreateRegistries.DISPLAY_TARGET, supplier
-		).byBlock(DisplayTarget.BY_BLOCK).byBlockEntity(DisplayTarget.BY_BLOCK_ENTITY));
+		return this.entry(name, callback -> new SimpleBuilder<>(this, this, name, callback, CreateRegistries.DISPLAY_TARGET, supplier).byBlock(DisplayTarget.BY_BLOCK).byBlockEntity(DisplayTarget.BY_BLOCK_ENTITY));
 	}
 
-	/* Palettes */
-
-	public <T extends Block> BlockBuilder<T, CreateRegistrate> paletteStoneBlock(String name,
-																				 NonNullFunction<Properties, T> factory, NonNullSupplier<Block> propertiesFrom, boolean worldGenStone,
-																				 boolean hasNaturalVariants) {
+	public <T extends Block> BlockBuilder<T, CreateRegistrate> paletteStoneBlock(String name, NonNullFunction<Properties, T> factory, NonNullSupplier<Block> propertiesFrom, boolean worldGenStone, boolean hasNaturalVariants) {
 		BlockBuilder<T, CreateRegistrate> builder = super.block(name, factory).initialProperties(propertiesFrom)
 			.transform(pickaxeOnly())
 			.blockstate(hasNaturalVariants ? BlockStateGen.naturalStoneTypeBlock(name) : (c, p) -> {
 				final String location = "block/palettes/stone_types/" + c.getName();
-				p.simpleBlock(c.get(), p.models()
-					.cubeAll(c.getName(), p.modLoc(location)));
+				p.simpleBlock(c.get(), p.models().cubeAll(c.getName(), p.modLoc(location)));
 			})
 			.tag(BlockTags.DRIPSTONE_REPLACEABLE)
 			.tag(BlockTags.AZALEA_ROOT_REPLACEABLE)
 			.tag(BlockTags.MOSS_REPLACEABLE)
 			.tag(BlockTags.LUSH_GROUND_REPLACEABLE)
 			.item()
-			.model((c, p) -> p.cubeAll(c.getName(),
-				p.modLoc(hasNaturalVariants ? "block/palettes/stone_types/natural/" + name + "_1"
-					: "block/palettes/stone_types/" + c.getName())))
+			.model((c, p) -> p.cubeAll(c.getName(), p.modLoc(hasNaturalVariants ? "block/palettes/stone_types/natural/" + name + "_1" : "block/palettes/stone_types/" + c.getName())))
 			.build();
 		return builder;
 	}
 
-	public BlockBuilder<Block, CreateRegistrate> paletteStoneBlock(String name, NonNullSupplier<Block> propertiesFrom,
-																   boolean worldGenStone, boolean hasNaturalVariants) {
+	public BlockBuilder<Block, CreateRegistrate> paletteStoneBlock(String name, NonNullSupplier<Block> propertiesFrom, boolean worldGenStone, boolean hasNaturalVariants) {
 		return paletteStoneBlock(name, Block::new, propertiesFrom, worldGenStone, hasNaturalVariants);
 	}
 
-	/* Fluids */
-
 	public <T extends BaseFlowingFluid> FluidBuilder<T, CreateRegistrate> virtualFluid(String name,
-																					   FluidBuilder.FluidTypeFactory typeFactory, NonNullFunction<BaseFlowingFluid.Properties, T> sourceFactory,
-																					   NonNullFunction<BaseFlowingFluid.Properties, T> flowingFactory) {
-		return entry(name,
-			c -> new VirtualFluidBuilder<>(self(), self(), name, c, ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"),
-				ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"), typeFactory, sourceFactory, flowingFactory));
+			FluidBuilder.FluidTypeFactory typeFactory, NonNullFunction<BaseFlowingFluid.Properties, T> sourceFactory,
+			NonNullFunction<BaseFlowingFluid.Properties, T> flowingFactory) {
+		return entry(name, c -> new VirtualFluidBuilder<>(self(), self(), name, c, Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"),
+			Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"), typeFactory, sourceFactory, flowingFactory));
 	}
 
 	public <T extends BaseFlowingFluid> FluidBuilder<T, CreateRegistrate> virtualFluid(String name,
-																						ResourceLocation still, ResourceLocation flow, FluidBuilder.FluidTypeFactory typeFactory,
-																						NonNullFunction<BaseFlowingFluid.Properties, T> sourceFactory, NonNullFunction<BaseFlowingFluid.Properties, T> flowingFactory) {
+			Identifier still, Identifier flow, FluidBuilder.FluidTypeFactory typeFactory,
+			NonNullFunction<BaseFlowingFluid.Properties, T> sourceFactory, NonNullFunction<BaseFlowingFluid.Properties, T> flowingFactory) {
 		return entry(name, c -> new VirtualFluidBuilder<>(self(), self(), name, c, still, flow, typeFactory, sourceFactory, flowingFactory));
 	}
 
 	public FluidBuilder<VirtualFluid, CreateRegistrate> virtualFluid(String name) {
-		return entry(name,
-			c -> new VirtualFluidBuilder<>(self(), self(), name, c,
-				ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"),
-				CreateRegistrate::defaultFluidType, VirtualFluid::createSource, VirtualFluid::createFlowing));
+		return entry(name, c -> new VirtualFluidBuilder<>(self(), self(), name, c,
+			Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"),
+			CreateRegistrate::defaultFluidType, VirtualFluid::createSource, VirtualFluid::createFlowing));
 	}
 
-	public FluidBuilder<VirtualFluid, CreateRegistrate> virtualFluid(String name, ResourceLocation still,
-																	 ResourceLocation flow) {
+	public FluidBuilder<VirtualFluid, CreateRegistrate> virtualFluid(String name, Identifier still, Identifier flow) {
 		return entry(name, c -> new VirtualFluidBuilder<>(self(), self(), name, c, still, flow,
 			CreateRegistrate::defaultFluidType, VirtualFluid::createSource, VirtualFluid::createFlowing));
 	}
 
 	public FluidBuilder<BaseFlowingFluid.Flowing, CreateRegistrate> standardFluid(String name) {
-		return fluid(name, ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"));
+		return fluid(name, Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"));
 	}
 
-	public FluidBuilder<BaseFlowingFluid.Flowing, CreateRegistrate> standardFluid(String name,
-																				   FluidBuilder.FluidTypeFactory typeFactory) {
-		return fluid(name, ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), ResourceLocation.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"),
-			typeFactory);
+	public FluidBuilder<BaseFlowingFluid.Flowing, CreateRegistrate> standardFluid(String name, FluidBuilder.FluidTypeFactory typeFactory) {
+		return fluid(name, Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_still"), Identifier.fromNamespaceAndPath(getModid(), "fluid/" + name + "_flow"), typeFactory);
 	}
 
-	public static FluidType defaultFluidType(FluidType.Properties properties, ResourceLocation stillTexture,
-											 ResourceLocation flowingTexture) {
+	public static FluidType defaultFluidType(FluidType.Properties properties, Identifier stillTexture, Identifier flowingTexture) {
 		return new FluidType(properties) {
 			@Override
 			public void initializeClient(Consumer<IClientFluidTypeExtensions> consumer) {
 				consumer.accept(new IClientFluidTypeExtensions() {
 					@Override
-					public ResourceLocation getStillTexture() {
+					public Identifier getStillTexture() {
 						return stillTexture;
 					}
 
 					@Override
-					public ResourceLocation getFlowingTexture() {
+					public Identifier getFlowingTexture() {
 						return flowingTexture;
 					}
 				});
@@ -267,52 +233,28 @@ public class CreateRegistrate extends AbstractRegistrate<CreateRegistrate> {
 		};
 	}
 
-	/* Util */
-
-	public static <T extends Block> NonNullConsumer<? super T> casingConnectivity(
-		BiConsumer<T, CasingConnectivity> consumer) {
-		return entry -> CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> registerCasingConnectivity(entry, consumer));
-	}
-
-	public static <T extends Block> NonNullConsumer<? super T> blockModel(
-		Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
-		return entry -> CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> registerBlockModel(entry, func));
-	}
-
-	public static <T extends Item> NonNullConsumer<? super T> itemModel(
-		Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
-		return entry -> CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> registerItemModel(entry, func));
-	}
-
-	public static NonNullConsumer<? super Block> connectedTextures(
-		Supplier<ConnectedTextureBehaviour> behavior) {
+	public static NonNullConsumer<? super Block> connectedTextures(Supplier<ConnectedTextureBehaviour> behavior) {
 		return entry -> CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> registerCTBehviour(entry, behavior));
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	private static <T extends Block> void registerCasingConnectivity(T entry,
-																	 BiConsumer<T, CasingConnectivity> consumer) {
+	private static <T extends Block> void registerCasingConnectivity(T entry, BiConsumer<T, CasingConnectivity> consumer) {
 		consumer.accept(entry, CreateClient.CASING_CONNECTIVITY);
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	private static void registerBlockModel(Block entry,
-										   Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
-		CreateClient.MODEL_SWAPPER.getCustomBlockModels()
-			.register(RegisteredObjectsHelper.getKeyOrThrow(entry), func.get());
+	private static void registerBlockModel(Block entry, Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
+		CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(RegisteredObjectsHelper.getKeyOrThrow(entry), func.get());
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	private static void registerItemModel(Item entry,
-										  Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
-		CreateClient.MODEL_SWAPPER.getCustomItemModels()
-			.register(RegisteredObjectsHelper.getKeyOrThrow(entry), func.get());
+	private static void registerItemModel(Item entry, Supplier<NonNullFunction<BakedModel, ? extends BakedModel>> func) {
+		CreateClient.MODEL_SWAPPER.getCustomItemModels().register(RegisteredObjectsHelper.getKeyOrThrow(entry), func.get());
 	}
 
 	@OnlyIn(Dist.CLIENT)
 	private static void registerCTBehviour(Block entry, Supplier<ConnectedTextureBehaviour> behaviorSupplier) {
 		ConnectedTextureBehaviour behavior = behaviorSupplier.get();
-		CreateClient.MODEL_SWAPPER.getCustomBlockModels()
-			.register(RegisteredObjectsHelper.getKeyOrThrow(entry), model -> new CTModel(model, behavior));
+		CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(RegisteredObjectsHelper.getKeyOrThrow(entry), model -> new CTModel(model, behavior));
 	}
 }
