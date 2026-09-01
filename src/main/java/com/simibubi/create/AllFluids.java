50421c26e08babcca158ef7d9463b3868ea5d225
bladenowain-arch <bladenowain@gmail.com>
2026-09-01 21:43:01 +0100

    Restore Color import for fluid rendering migration

--- src/main/java/com/simibubi/create/AllFluids.java
+++ src/main/java/com/simibubi/create/AllFluids.java
@@ -21,6 +21,7 @@ import com.tterrag.registrate.builders.FluidBuilder.FluidTypeFactory;
 import com.tterrag.registrate.util.entry.FluidEntry;
 
 import net.createmod.catnip.lang.Lang;
+import net.createmod.catnip.theme.Color;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.FogRenderer.FogMode;
@@ -73,14 +74,10 @@ public class AllFluids {
 				SolidRenderedPlaceableFluidType.create(0xEAAE2F,
 					() -> 1f / 8f * AllConfigs.client().honeyTransparencyMultiplier.getF()))
 			.lang("Honey")
-			.properties(b -> b.viscosity(2000)
-				.density(1400))
-			.fluidProperties(p -> p.levelDecreasePerBlock(2)
-				.tickRate(25)
-				.slopeFindDistance(3)
-				.explosionResistance(100f))
+			.properties(b -> b.viscosity(2000).density(1400))
+			.fluidProperties(p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(3).explosionResistance(100f))
 			.tag(Tags.Fluids.HONEY)
-			.source(BaseFlowingFluid.Source::new) // TODO: remove when Registrate fixes FluidBuilder
+			.source(BaseFlowingFluid.Source::new)
 			.block()
 			.properties(p -> p.mapColor(MapColor.TERRACOTTA_YELLOW))
 			.build()
@@ -96,12 +93,8 @@ public class AllFluids {
 					() -> 1f / 32f * AllConfigs.client().chocolateTransparencyMultiplier.getF()))
 			.lang("Chocolate")
 			.tag(AllFluidTags.CHOCOLATE.tag)
-			.properties(b -> b.viscosity(1500)
-				.density(1400))
-			.fluidProperties(p -> p.levelDecreasePerBlock(2)
-				.tickRate(25)
-				.slopeFindDistance(3)
-				.explosionResistance(100f))
+			.properties(b -> b.viscosity(1500).density(1400))
+			.fluidProperties(p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(3).explosionResistance(100f))
 			.source(BaseFlowingFluid.Source::new)
 			.block()
 			.properties(p -> p.mapColor(MapColor.TERRACOTTA_BROWN))
@@ -112,73 +105,40 @@ public class AllFluids {
 			.build()
 			.register();
 
-	// Load this class
-
-	public static void register() {
-	}
+	public static void register() {}
 
 	public static void registerFluidInteractions() {
-		FluidInteractionRegistry.addInteraction(NeoForgeMod.LAVA_TYPE.value(), new InteractionInformation(
-			HONEY.get().getFluidType(),
-			fluidState -> {
-				if (fluidState.isSource()) {
-					return Blocks.OBSIDIAN.defaultBlockState();
-				} else {
-					return AllPaletteStoneTypes.LIMESTONE.getBaseBlock()
-						.get()
-						.defaultBlockState();
-				}
-			}
-		));
-
-		FluidInteractionRegistry.addInteraction(NeoForgeMod.LAVA_TYPE.value(), new InteractionInformation(
-			CHOCOLATE.get().getFluidType(),
-			fluidState -> {
-				if (fluidState.isSource()) {
-					return Blocks.OBSIDIAN.defaultBlockState();
-				} else {
-					return AllPaletteStoneTypes.SCORIA.getBaseBlock()
-						.get()
-						.defaultBlockState();
-				}
-			}
-		));
+		FluidInteractionRegistry.addInteraction(NeoForgeMod.LAVA_TYPE.value(), new InteractionInformation(HONEY.get().getFluidType(),
+			fluidState -> fluidState.isSource() ? Blocks.OBSIDIAN.defaultBlockState() : AllPaletteStoneTypes.LIMESTONE.getBaseBlock().get().defaultBlockState()));
+		FluidInteractionRegistry.addInteraction(NeoForgeMod.LAVA_TYPE.value(), new InteractionInformation(CHOCOLATE.get().getFluidType(),
+			fluidState -> fluidState.isSource() ? Blocks.OBSIDIAN.defaultBlockState() : AllPaletteStoneTypes.SCORIA.getBaseBlock().get().defaultBlockState()));
 	}
 
 	@Nullable
 	public static BlockState getLavaInteraction(FluidState fluidState) {
 		Fluid fluid = fluidState.getType();
-		if (fluid.isSame(HONEY.get()))
-			return AllPaletteStoneTypes.LIMESTONE.getBaseBlock()
-				.get()
-				.defaultBlockState();
-		if (fluid.isSame(CHOCOLATE.get()))
-			return AllPaletteStoneTypes.SCORIA.getBaseBlock()
-				.get()
-				.defaultBlockState();
+		if (fluid.isSame(HONEY.get())) return AllPaletteStoneTypes.LIMESTONE.getBaseBlock().get().defaultBlockState();
+		if (fluid.isSame(CHOCOLATE.get())) return AllPaletteStoneTypes.SCORIA.getBaseBlock().get().defaultBlockState();
 		return null;
 	}
 
 	private static final DispenseItemBehavior DEFAULT = new DefaultDispenseItemBehavior();
-	private static final DispenseItemBehavior DISPENSE_FLUID = new DefaultDispenseItemBehavior(){
-			@Override
-			protected ItemStack execute(BlockSource pSource, ItemStack pStack) {
-				DispensibleContainerItem dispensibleContainerItem = (DispensibleContainerItem) pStack.getItem();
-				BlockPos pos = pSource.pos().relative(pSource.state().getValue(DispenserBlock.FACING));
-				Level level = pSource.level();
-				if (dispensibleContainerItem.emptyContents(null, level, pos, null, pStack)) {
-					return new ItemStack(Items.BUCKET);
-				}
-				return DEFAULT.dispense(pSource, pStack);
-			}
-		};
+	private static final DispenseItemBehavior DISPENSE_FLUID = new DefaultDispenseItemBehavior() {
+		@Override
+		protected ItemStack execute(BlockSource source, ItemStack stack) {
+			DispensibleContainerItem container = (DispensibleContainerItem) stack.getItem();
+			BlockPos pos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
+			Level level = source.level();
+			if (container.emptyContents(null, level, pos, null, stack)) return new ItemStack(Items.BUCKET);
+			return DEFAULT.dispense(source, stack);
+		}
+	};
 
 	private static void registerFluidDispenseBehavior(BucketItem bucket) {
 		DispenserBlock.registerBehavior(bucket, DISPENSE_FLUID);
 	}
 
 	public static abstract class TintedFluidType extends FluidType {
-
 		protected static final int NO_TINT = 0xffffffff;
 		private final Identifier stillTexture;
 		private final Identifier flowingTexture;
@@ -192,65 +152,32 @@ public class AllFluids {
 		@Override
 		public void initializeClient(Consumer<IClientFluidTypeExtensions> consumer) {
 			consumer.accept(new IClientFluidTypeExtensions() {
-
-				@Override
-				public Identifier getStillTexture() {
-					return stillTexture;
-				}
-
-				@Override
-				public Identifier getFlowingTexture() {
-					return flowingTexture;
-				}
-
-				@Override
-				public int getTintColor(FluidStack stack) {
-					return TintedFluidType.this.getTintColor(stack);
-				}
-
-				@Override
-				public int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) {
-					return TintedFluidType.this.getTintColor(state, getter, pos);
-				}
-
-				@Override
-				public @NotNull Vector3f modifyFogColor(Camera camera, float partialTick, ClientLevel level,
-														int renderDistance, float darkenWorldAmount, Vector3f fluidFogColor) {
+				@Override public Identifier getStillTexture() { return stillTexture; }
+				@Override public Identifier getFlowingTexture() { return flowingTexture; }
+				@Override public int getTintColor(FluidStack stack) { return TintedFluidType.this.getTintColor(stack); }
+				@Override public int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) { return TintedFluidType.this.getTintColor(state, getter, pos); }
+				@Override public @NotNull Vector3f modifyFogColor(Camera camera, float partialTick, ClientLevel level, int renderDistance, float darkenWorldAmount, Vector3f fluidFogColor) {
 					Vector3f customFogColor = TintedFluidType.this.getCustomFogColor();
 					return customFogColor == null ? fluidFogColor : customFogColor;
 				}
-
-				@Override
-				public void modifyFogRender(Camera camera, FogMode mode, float renderDistance, float partialTick,
-											float nearDistance, float farDistance, FogShape shape) {
+				@Override public void modifyFogRender(Camera camera, FogMode mode, float renderDistance, float partialTick, float nearDistance, float farDistance, FogShape shape) {
 					float modifier = TintedFluidType.this.getFogDistanceModifier();
-					float baseWaterFog = 96.0f;
 					if (modifier != 1f) {
 						RenderSystem.setShaderFogShape(FogShape.CYLINDER);
 						RenderSystem.setShaderFogStart(-8);
-						RenderSystem.setShaderFogEnd(baseWaterFog * modifier);
+						RenderSystem.setShaderFogEnd(96.0f * modifier);
 					}
 				}
-
 			});
 		}
 
 		protected abstract int getTintColor(FluidStack stack);
-
 		protected abstract int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos);
-
-		protected Vector3f getCustomFogColor() {
-			return null;
-		}
-
-		protected float getFogDistanceModifier() {
-			return 1f;
-		}
-
+		protected Vector3f getCustomFogColor() { return null; }
+		protected float getFogDistanceModifier() { return 1f; }
 	}
 
 	private static class SolidRenderedPlaceableFluidType extends TintedFluidType {
-
 		private Vector3f fogColor;
 		private Supplier<Float> fogDistance;
 
@@ -263,36 +190,13 @@ public class AllFluids {
 			};
 		}
 
-		private SolidRenderedPlaceableFluidType(Properties properties, Identifier stillTexture,
-												Identifier flowingTexture) {
+		private SolidRenderedPlaceableFluidType(Properties properties, Identifier stillTexture, Identifier flowingTexture) {
 			super(properties, stillTexture, flowingTexture);
 		}
 
-		@Override
-		protected int getTintColor(FluidStack stack) {
-			return NO_TINT;
-		}
-
-		/*
-		 * Removing alpha from tint prevents optifine from forcibly applying biome
-		 * colors to modded fluids (this workaround only works for fluids in the solid
-		 * render layer)
-		 */
-		@Override
-		public int getTintColor(FluidState state, BlockAndTintGetter world, BlockPos pos) {
-			return 0x00ffffff;
-		}
-
-		@Override
-		protected Vector3f getCustomFogColor() {
-			return fogColor;
-		}
-
-		@Override
-		protected float getFogDistanceModifier() {
-			return fogDistance.get();
-		}
-
+		@Override protected int getTintColor(FluidStack stack) { return NO_TINT; }
+		@Override protected int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) { return NO_TINT; }
+		@Override protected Vector3f getCustomFogColor() { return fogColor; }
+		@Override protected float getFogDistanceModifier() { return fogDistance.get(); }
 	}
-
 }
