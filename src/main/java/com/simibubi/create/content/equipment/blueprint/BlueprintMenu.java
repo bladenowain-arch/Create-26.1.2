9ab3f8f64c5877e08e454699e8115251749f6f49
bladenowain-arch <bladenowain@gmail.com>
2026-09-02 17:16:23 +0100

    Use 26.1 recipe access generics in blueprint menu

--- src/main/java/com/simibubi/create/content/equipment/blueprint/BlueprintMenu.java
+++ src/main/java/com/simibubi/create/content/equipment/blueprint/BlueprintMenu.java
@@ -15,7 +15,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.TransientCraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
@@ -72,7 +71,7 @@ public class BlueprintMenu extends GhostItemMenu<BlueprintSection> {
 
 		ServerPlayer serverplayerentity = (ServerPlayer) player;
 		CraftingContainer craftingInventory = new BlueprintCraftingInventory(this, ghostInventory);
-		Optional<RecipeHolder<CraftingRecipe>> optional = level.recipeAccess()
+		Optional<RecipeHolder<? extends CraftingRecipe>> optional = level.recipeAccess()
 			.getRecipeFor(RecipeType.CRAFTING, craftingInventory.asCraftInput(), level);
 
 		if (!optional.isPresent()) {
