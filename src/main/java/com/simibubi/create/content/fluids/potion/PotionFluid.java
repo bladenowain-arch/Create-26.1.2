e0bdab9fc999f567dfdb6bc282680c1acc57f77a
bladenowain-arch <bladenowain@gmail.com>
2026-09-02 11:04:33 +0100

    Migrate potion fluid identifiers to 26.1

--- src/main/java/com/simibubi/create/content/fluids/potion/PotionFluid.java
+++ src/main/java/com/simibubi/create/content/fluids/potion/PotionFluid.java
@@ -14,7 +14,7 @@ import net.createmod.catnip.lang.Lang;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionContents;
@@ -69,7 +69,7 @@ public class PotionFluid extends VirtualFluid {
 
 	public static class PotionFluidType extends TintedFluidType {
 
-		public PotionFluidType(net.neoforged.neoforge.fluids.FluidType.Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
+		public PotionFluidType(net.neoforged.neoforge.fluids.FluidType.Properties properties, Identifier stillTexture, Identifier flowingTexture) {
 			super(properties, stillTexture, flowingTexture);
 		}
 
